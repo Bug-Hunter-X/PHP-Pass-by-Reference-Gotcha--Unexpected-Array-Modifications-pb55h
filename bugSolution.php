7df5665b617574b819c85d40ca889eb9652a96da
BugHunter-X <askzenai@gmail.com>
2025-01-01 05:50:46 +0100

    Add bugSolution.php file

---
+++ bugSolution.php
@@ -0,0 +1,27 @@
+```php
+function processArray(array &$arr) {
+  foreach ($arr as &$value) {
+    $value = $value * 2; 
+  }
+}
+
+function modifyArray(array &$arr) {
+    foreach ($arr as $key => &$value) {
+        $value = $value * 2; 
+    }
+}
+
+$myArray = [1, 2, 3, 4, 5];
+modifyArray($myArray);
+print_r($myArray); // Output: Array ( [0] => 2 [1] => 4 [2] => 6 [3] => 8 [4] => 10 )
+
+$myArray = [1, 2, 3, 4, 5];
+$myArray2 = $myArray; // Assign by value
+modifyArray($myArray2);
+print_r($myArray); // Output: Array ( [0] => 1 [1] => 2 [2] => 3 [3] => 4 [4] => 5 )
+
+$myArray = [1, 2, 3, 4, 5];
+$myArray2 = &$myArray; // Assign by reference
+modifyArray($myArray2);
+print_r($myArray); // Output: Array ( [0] => 2 [1] => 4 [2] => 6 [3] => 8 [4] => 10 )
+```
